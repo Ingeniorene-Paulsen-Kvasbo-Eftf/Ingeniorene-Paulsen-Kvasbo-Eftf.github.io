62b1d3d975fb344e912bb6d47c0ad64e45f30167
Audun Kvasbø <audun@kvasbo.no>
2020-08-26 21:44:13 +0200

    Eliza!

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,26 @@
-window.onload = function () { };
+var terminal = new Terminal();
+var eliza = new ElizaBot();
+window.onload = function () {
+    var initial = eliza.getInitial();
+    terminal.setTextColor("#ffffff");
+    terminal.setWidth("100%");
+    terminal.setHeight("100%");
+    terminal.setBackgroundColor("#00000000");
+    var container = document.getElementById("terminal_active");
+    container.appendChild(terminal.html);
+    terminal.input(initial, handleElizaReply);
+};
+function handleElizaReply(input) {
+    var reply = eliza.transform(input);
+    if (eliza.quit) {
+        var final = eliza.getFinal();
+        terminal.clear();
+        terminal.print(final);
+        terminal.beep();
+        return;
+    }
+    else {
+        terminal.clear();
+        terminal.input(reply, handleElizaReply);
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
-window.onload = function () {};
+declare var Terminal: any;
+declare var ElizaBot: any;
+
+const terminal = new Terminal();
+const eliza = new ElizaBot();
+
+window.onload = function () {
+  // Init libs
+
+  const initial = eliza.getInitial();
+
+  // Set up terminal
+  terminal.setTextColor("#ffffff");
+  terminal.setWidth("100%");
+  terminal.setHeight("100%");
+  terminal.setBackgroundColor("#00000000");
+  const container = document.getElementById("terminal_active");
+  container.appendChild(terminal.html);
+
+  terminal.input(initial, handleElizaReply);
+};
+
+function handleElizaReply(input: string) {
+  const reply = eliza.transform(input);
+  if (eliza.quit) {
+    const final = eliza.getFinal();
+    terminal.clear();
+    terminal.print(final);
+    terminal.beep();
+    return;
+  } else {
+    terminal.clear();
+    terminal.input(reply, handleElizaReply);
+  }
+}
